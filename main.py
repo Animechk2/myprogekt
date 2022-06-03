dffa5c699a6c6b534adbe4dac742d96a7a514a52
Animechk2 <105502935+Animechk2@users.noreply.github.com>
2022-06-03 19:23:00 +0300

    generator

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-while True: print(eval(input(">>>")))
+class Counter:
+    def _int_ (self, max_number):
+        self.i = 0
+        self.max_number = max_number
+    def __iter__(self):
+        self.i = 0
+        return self
+    def __next__(self):
+        self.i += 1
+        if self.i > self.max_number:
+            raise StopIteration
+        return self.i
+count = Counter(10)
+for elem in count:
+    print(elem)
